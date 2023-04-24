b9543ee63bae7493ec1b2bd8db43d2afef3695f4
Vijay Bagavatula <vijaypb024@gmail.com>
2023-04-24 16:41:50 -0400

    added buttons to lobby main for transitions

--- app/src/main/java/com/example/trivia_night/lobby_main.java
+++ app/src/main/java/com/example/trivia_night/lobby_main.java
@@ -2,13 +2,58 @@ package com.example.trivia_night;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class lobby_main extends AppCompatActivity {
+    ImageView calender;
+    ImageView profile;
+    ImageView search;
+    ImageView home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby_main);
+        calender = findViewById(R.id.calender);
+        home = findViewById(R.id.home);
+        search = findViewById(R.id.search);
+        profile = findViewById(R.id.profile);
+        calender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event for imageView1
+                Intent intent = new Intent(lobby_main.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event for imageView2
+                Intent intent = new Intent(lobby_main.this, home.class);
+                startActivity(intent);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event for imageView3
+                Intent intent = new Intent(lobby_main.this, search_friend.class);
+                startActivity(intent);
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event for imageView3
+                Intent intent = new Intent(lobby_main.this, Profile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
